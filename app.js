85bf63fa5357092c5520ca02380b15c20bdfa392
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-06-25 18:20:34 -0400

    style sheet added

--- app.js
+++ app.js
@@ -138,10 +138,19 @@ promptUser()
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw (err);
-
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+      if (err) {
+        return;
+      }
       console.log('Page created! Checkout index.html to see it!');
+
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Style sheet copied successfully!');
+      });
     });
   });
 
